24aec0de90a86d80e5576d013de964318dc240ba
chaparkcha <chaparkcha@gmail.com>
2022-05-01 13:34:34 +0900

    vuetifyjs 3 beta 설치

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api': { 
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }, 
+    },
+  },
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 })
